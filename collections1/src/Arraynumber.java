0587d0ae73e097ab871df17251bb22c115ba4187
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-02 18:56:05 +0530

    5th commit
    Uplod all files

---
+++ collections1/src/Arraynumber.java
@@ -0,0 +1,16 @@
+import  java.util.*;
+public class Arraynumber {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>numbers=new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		System.out.println("number of elements in ArrrayList:"  +numbers.size());
+		
+	}
+		
+
+	}
+
+
---
+++ collections1/src/collections1/C.java
@@ -0,0 +1,10 @@
+package collections1;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
